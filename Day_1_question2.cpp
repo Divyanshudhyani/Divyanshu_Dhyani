c633470061a9208087ef21fe0f33a759d9a3da82
Divyanshudhyani <149132859+Divyanshudhyani@users.noreply.github.com>
2024-06-21 19:42:02 +0530

    Create Day_1_question2.cpp

---
+++ Day_1_question2.cpp
@@ -0,0 +1,11 @@
+int main(){
+  int ans;
+  for(int i = 0 ; i < arr.size() ; i++){
+    for(int j =i+1 ; j < arr.size() ; j++){
+      if(i<j && (((i+j)%k)==0){
+        ans++;
+      }
+    }
+  }
+  return ans;
+}
